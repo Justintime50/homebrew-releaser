8c81e02927ef25e32a045905deed803975b4bf78
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2021-04-27 19:47:49 -0600

    v0.5.3 - bug fixes

--- homebrew_releaser/releaser.py
+++ homebrew_releaser/releaser.py
@@ -212,7 +212,8 @@ def setup_git(commit_owner, commit_email, homebrew_owner, homebrew_tap):
     try:
         output = subprocess.check_output(
             (
-                f'git config user.name "{commit_owner}"'
+                f'cd {homebrew_tap}'
+                f' && git config user.name "{commit_owner}"'
                 f' && git config user.email {commit_email}'
                 f' && git clone --depth=2 https://{GITHUB_TOKEN}@github.com/{homebrew_owner}/{homebrew_tap}.git'
             ),
