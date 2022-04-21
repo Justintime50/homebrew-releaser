b8b42999092e8961ac1f1ee460c04b439ea5d1d7
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2022-04-21 00:23:40 -0600

    fix: pin deps

--- setup.py
+++ setup.py
@@ -10,13 +10,15 @@ REQUIREMENTS = [
 ]
 
 DEV_REQUIREMENTS = [
-    'black',
+    'black == 22.*',
+    'build == 0.7.*',
     'coveralls == 3.*',
-    'flake8',
-    'isort',
-    'mypy',
-    'pytest == 6.*',
-    'pytest-cov == 2.*',
+    'flake8 == 4.*',
+    'isort == 5.*',
+    'mypy == 0.942',
+    'pytest == 7.*',
+    'pytest-cov == 3.*',
+    'twine == 4.*',
     'types-requests',
 ]
 
