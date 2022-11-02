8c6c99866d1e331146846b4d7da211313a0c686f
Alec Reynolds <alec+git@lando.dev>
2022-11-02 06:16:06 -0700

    #9: Update constants correctly considering GitHub booleans.

--- homebrew_releaser/constants.py
+++ homebrew_releaser/constants.py
@@ -1,5 +1,16 @@
 import os
 
+# Helper function to translate given INPUT_TARGET_* string from GitHub Actions into
+# appropriate bool or string value.
+def translate_target(target):
+  if isinstance(target, (bool)):
+    return bool
+
+  # Must check for string `false`/`true` since GitHub Actions passes the bool as a string
+  if target.lower() not in set('true', 'false'):
+    return target
+  else:
+    return target.lower() == 'true'
 
 # User Input
 FORMULA_FOLDER = os.getenv('INPUT_FORMULA_FOLDER', 'formula')
@@ -24,15 +35,7 @@ GITHUB_OWNER = GITHUB_REPOSITORY[0]
 GITHUB_REPO = GITHUB_REPOSITORY[1]
 
 # Matrix targets to add URL/checksum targets for
-TARGET_DARWIN_AMD64 = (
-    os.getenv('INPUT_TARGET_DARWIN_AMD64', False) if os.getenv('INPUT_TARGET_DARWIN_AMD64') != 'false' else False
-)  # Must check for string `false` since GitHub Actions passes the bool as a string
-TARGET_DARWIN_ARM64 = (
-    os.getenv('INPUT_TARGET_DARWIN_ARM64', False) if os.getenv('INPUT_TARGET_DARWIN_ARM64') != 'false' else False
-)  # Must check for string `false` since GitHub Actions passes the bool as a string
-TARGET_LINUX_AMD64 = (
-    os.getenv('INPUT_TARGET_LINUX_AMD64', False) if os.getenv('INPUT_TARGET_LINUX_AMD64') != 'false' else False
-)  # Must check for string `false` since GitHub Actions passes the bool as a string
-TARGET_LINUX_ARM64 = (
-    os.getenv('INPUT_TARGET_LINUX_ARM64', False) if os.getenv('INPUT_TARGET_LINUX_ARM64') != 'false' else False
-)  # Must check for string `false` since GitHub Actions passes the bool as a string
+TARGET_DARWIN_AMD64 = translate_target(os.getenv('INPUT_TARGET_DARWIN_AMD64', False))
+TARGET_DARWIN_ARM64 = translate_target(os.getenv('INPUT_TARGET_DARWIN_ARM64', False))
+TARGET_LINUX_AMD64 = translate_target(os.getenv('INPUT_TARGET_LINUX_AMD64', False))
+TARGET_LINUX_ARM64 = translate_target(os.getenv('INPUT_TARGET_LINUX_ARM64', False))
