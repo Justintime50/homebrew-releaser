0f80d319e73a7a3d9f35fe3711439e07f30887b4
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2022-11-02 18:55:43 -0600

    fix: lcov generation

--- setup.py
+++ setup.py
@@ -15,12 +15,11 @@ DEV_REQUIREMENTS = [
     'bandit == 1.7.*',
     'black == 22.*',
     'build == 0.8.*',
-    'coveralls == 3.*',
     'flake8 == 5.*',
     'isort == 5.*',
     'mypy == 0.971',
     'pytest == 7.*',
-    'pytest-cov == 3.*',
+    'pytest-cov == 4.*',
     'twine == 4.*',
     'types-requests',
 ]
