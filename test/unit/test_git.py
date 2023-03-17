19a3ba7ddb7fd14bde3e2388175e4ac1d51cc4e0
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2023-03-16 23:24:49 -0600

    fix: tests

--- test/unit/test_git.py
+++ test/unit/test_git.py
@@ -13,6 +13,7 @@ from homebrew_releaser.git import Git
 @patch('homebrew_releaser.git.GITHUB_TOKEN', '123')
 @patch('subprocess.check_output')
 def test_setup(mock_subprocess):
+    """Tests that we call the correct subprocess commands when setting up the git environment."""
     homebrew_owner = 'Justintime50'
     homebrew_tap = 'homebrew-formulas'
     commit_email = 'user@example.com'
@@ -27,79 +28,43 @@ def test_setup(mock_subprocess):
                     '--depth=1',
                     'https://x-access-token:123@github.com/Justintime50/homebrew-formulas.git',
                 ],
-                stdin=None,
+                stderr=-2,
+                text=True,
                 timeout=30,
             ),
             call(
                 ['git', '-C', 'homebrew-formulas', 'config', 'user.name', '"Justintime50"'],
-                stdin=None,
+                stderr=-2,
+                text=True,
                 timeout=30,
             ),
             call(
                 ['git', '-C', 'homebrew-formulas', 'config', 'user.email', 'user@example.com'],
-                stdin=None,
+                stderr=-2,
+                text=True,
                 timeout=30,
             ),
         ]
     )
 
 
-@patch(
-    'subprocess.check_output',
-    side_effect=subprocess.TimeoutExpired(cmd='subprocess.check_output', timeout=0.1),
-)
-def test_setup_subprocess_timeout(mock_subprocess):
-    homebrew_owner = 'Justintime50'
-    homebrew_tap = 'homebrew-formulas'
-    commit_email = 'user@example.com'
-    with pytest.raises(subprocess.TimeoutExpired):
-        Git.setup(homebrew_owner, commit_email, homebrew_owner, homebrew_tap)
-
-
-@patch(
-    'subprocess.check_output',
-    side_effect=subprocess.CalledProcessError(cmd='subprocess.check_output', returncode=1),
-)
-def test_setup_process_error(mock_subprocess):
-    homebrew_owner = 'Justintime50'
-    homebrew_tap = 'homebrew-formulas'
-    commit_email = 'user@example.com'
-    with pytest.raises(subprocess.CalledProcessError):
-        Git.setup(homebrew_owner, commit_email, homebrew_owner, homebrew_tap)
-
-
 @patch('subprocess.check_output')
 def test_add(mock_subprocess):
+    """Tests that we call the correct git add command."""
     homebrew_tap = 'homebrew-formulas'
 
     Git.add(homebrew_tap)
     mock_subprocess.assert_called_once_with(
         ['git', '-C', homebrew_tap, 'add', '.'],
-        stdin=None,
+        stderr=-2,
+        text=True,
         timeout=TIMEOUT,
     )
 
 
-@patch('subprocess.check_output', side_effect=subprocess.TimeoutExpired(cmd='subprocess.check_output', timeout=0.1))
-def test_add_subprocess_timeout(mock_subprocess):
-    homebrew_tap = 'homebrew-formulas'
-
-    with pytest.raises(subprocess.TimeoutExpired):
-        Git.add(homebrew_tap)
-
-
-@patch(
-    'subprocess.check_output', side_effect=subprocess.CalledProcessError(cmd='subprocess.check_output', returncode=1)
-)
-def test_add_process_error(mock_subprocess):
-    homebrew_tap = 'homebrew-formulas'
-
-    with pytest.raises(subprocess.CalledProcessError):
-        Git.add(homebrew_tap)
-
-
 @patch('subprocess.check_output')
 def test_commit(mock_subprocess):
+    """Tests that we call the correct git commit command."""
     homebrew_tap = 'homebrew-formulas'
     repo_name = 'mock-repo'
     version = '0.1.0'
@@ -107,36 +72,16 @@ def test_commit(mock_subprocess):
     Git.commit(homebrew_tap, repo_name, version)
     mock_subprocess.assert_called_once_with(
         ['git', '-C', homebrew_tap, 'commit', '-m', f'"Brew formula update for {repo_name} version {version}"'],
-        stdin=None,
+        stderr=-2,
+        text=True,
         timeout=TIMEOUT,
     )
-
-
-@patch('subprocess.check_output', side_effect=subprocess.TimeoutExpired(cmd='subprocess.check_output', timeout=0.1))
-def test_commit_subprocess_timeout(mock_subprocess):
-    homebrew_tap = 'homebrew-formulas'
-    repo_name = 'mock-repo'
-    version = '0.1.0'
-
-    with pytest.raises(subprocess.TimeoutExpired):
-        Git.commit(homebrew_tap, repo_name, version)
-
-
-@patch(
-    'subprocess.check_output', side_effect=subprocess.CalledProcessError(cmd='subprocess.check_output', returncode=1)
-)
-def test_commit_process_error(mock_subprocess):
-    homebrew_tap = 'homebrew-formulas'
-    repo_name = 'mock-repo'
-    version = '0.1.0'
-
-    with pytest.raises(subprocess.CalledProcessError):
-        Git.commit(homebrew_tap, repo_name, version)
 
 
 @patch('homebrew_releaser.git.GITHUB_TOKEN', '123')
 @patch('subprocess.check_output')
 def test_push(mock_subprocess):
+    """Tests that we call the correct git push command."""
     homebrew_tap = 'homebrew-formulas'
     homebrew_owner = 'Justintime50'
 
@@ -150,26 +95,39 @@ def test_push(mock_subprocess):
             'push',
             f'https://x-access-token:123@github.com/{homebrew_owner}/{homebrew_tap}.git',
         ],
-        stdin=None,
+        stderr=-2,
+        text=True,
         timeout=TIMEOUT,
     )
 
 
-@patch('subprocess.check_output', side_effect=subprocess.TimeoutExpired(cmd='subprocess.check_output', timeout=0.1))
-def test_push_subprocess_timeout(mock_subprocess):
-    homebrew_tap = 'homebrew-formulas'
-    homebrew_owner = 'Justintime50'
+@patch(
+    'subprocess.check_output',
+    side_effect=subprocess.CalledProcessError(cmd='subprocess.check_output', returncode=1),
+)
+def test_setup_called_process_error(mock_subprocess):
+    """Tests that we log a subprocess error when they happen.
 
-    with pytest.raises(subprocess.TimeoutExpired):
-        Git.push(homebrew_tap, homebrew_owner)
+    All git commands should fail in the same manner.
+    """
+    homebrew_owner = 'Justintime50'
+    homebrew_tap = 'homebrew-formulas'
+    commit_email = 'user@example.com'
+    with pytest.raises(subprocess.CalledProcessError):
+        Git.setup(homebrew_owner, commit_email, homebrew_owner, homebrew_tap)
 
 
 @patch(
-    'subprocess.check_output', side_effect=subprocess.CalledProcessError(cmd='subprocess.check_output', returncode=1)
+    'subprocess.check_output',
+    side_effect=Exception(),
 )
-def test_push_process_error(mock_subprocess):
-    homebrew_tap = 'homebrew-formulas'
-    homebrew_owner = 'Justintime50'
+def test_setup_exception(mock_subprocess):
+    """Tests that we log an exception when they happen.
 
-    with pytest.raises(subprocess.CalledProcessError):
-        Git.push(homebrew_tap, homebrew_owner)
+    All git commands should fail in the same manner.
+    """
+    homebrew_owner = 'Justintime50'
+    homebrew_tap = 'homebrew-formulas'
+    commit_email = 'user@example.com'
+    with pytest.raises(Exception):
+        Git.setup(homebrew_owner, commit_email, homebrew_owner, homebrew_tap)
