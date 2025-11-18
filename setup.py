e7abd727cb5e4644a0f8159746fb5cace0e3c32f
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2025-11-18 16:59:08 -0700

    chore: bump deps, try python 3.14 again

--- setup.py
+++ setup.py
@@ -2,48 +2,47 @@ import re
 
 import setuptools
 
-
 # Inspiration: https://stackoverflow.com/a/7071358/6064135
-with open('homebrew_releaser/_version.py', 'r') as version_file:
+with open("homebrew_releaser/_version.py", "r") as version_file:
     version_groups = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file.read(), re.M)
     if version_groups:
         version = version_groups.group(1)
     else:
-        raise RuntimeError('Unable to find version string!')
+        raise RuntimeError("Unable to find version string!")
 
 REQUIREMENTS = [
-    'chevron == 0.14.*',
-    'pretty-tables == 2.*',
-    'requests == 2.*',
-    'woodchips == 1.*',
+    "chevron == 0.14.*",
+    "pretty-tables == 4.*",
+    "requests == 2.*",
+    "woodchips == 2.*",
 ]
 
 DEV_REQUIREMENTS = [
-    'bandit == 1.8.*',
-    'black == 25.*',
-    'flake8 == 7.*',
-    'isort == 7.*',
-    'mypy == 1.18.*',
-    'pytest == 8.*',
-    'pytest-cov == 7.*',
+    "bandit == 1.9.*",
+    "black == 25.*",
+    "flake8 == 7.*",
+    "isort == 7.*",
+    "mypy == 1.18.*",
+    "pytest == 9.*",
+    "pytest-cov == 7.*",
 ]
 
 setuptools.setup(
-    name='homebrew-releaser',
+    name="homebrew-releaser",
     version=version,
-    description='Release scripts, binaries, and executables directly to Homebrew via GitHub Actions.',
-    url='http://github.com/Justintime50/homebrew-releaser',
-    author='Justintime50',
-    license='MIT',
+    description="Release scripts, binaries, and executables directly to Homebrew via GitHub Actions.",
+    url="http://github.com/Justintime50/homebrew-releaser",
+    author="Justintime50",
+    license="MIT",
     packages=setuptools.find_packages(
         exclude=[
-            'examples',
-            'test',
+            "examples",
+            "test",
         ]
     ),
     package_data={
-        'homebrew_releaser': [
-            'py.typed',
+        "homebrew_releaser": [
+            "py.typed",
         ]
     },
     classifiers=[
@@ -53,12 +52,12 @@ setuptools.setup(
     ],
     install_requires=REQUIREMENTS,
     extras_require={
-        'dev': DEV_REQUIREMENTS,
+        "dev": DEV_REQUIREMENTS,
     },
     entry_points={
-        'console_scripts': [
-            'homebrew-releaser=homebrew_releaser.releaser:main',
+        "console_scripts": [
+            "homebrew-releaser=homebrew_releaser.releaser:main",
         ]
     },
-    python_requires='==3.13.*',
+    python_requires="==3.14.*",
 )
