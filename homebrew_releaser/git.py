c57992084daaefe529dbd8066e3a16ff1d6e1d91
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2025-12-07 23:29:08 -0700

    fix: language of git command output

--- homebrew_releaser/git.py
+++ homebrew_releaser/git.py
@@ -41,14 +41,14 @@ def setup_git(commit_owner: str, commit_email: str, homebrew_owner: str, homebre
 def copy_formula_file_to_git(formula_filepath: str, homebrew_tap: str):
     """Copies the formula file from the official homebrew tap to the source code homebrew tap."""
     command = ["cp", formula_filepath, get_working_dir(homebrew_tap)]
-    _run_git_subprocess(command, "Formula file moved to git successfully.")
+    _run_git_subprocess(command, "Formula file moved to git repo successfully.")
 
 
 def add_git(homebrew_tap: str):
     """Adds git assets to a git commit."""
     # We add everything here because we updated the formula file and optionally the README
     command = ["git", "-C", get_working_dir(homebrew_tap), "add", "."]
-    _run_git_subprocess(command, "Formula file added to git commit successfully.")
+    _run_git_subprocess(command, "Assets added to git commit successfully.")
 
 
 def commit_git(homebrew_tap: str, repo_name: str, version: str):
@@ -56,7 +56,7 @@ def commit_git(homebrew_tap: str, repo_name: str, version: str):
     # fmt: off
     command = ['git', '-C', get_working_dir(homebrew_tap), 'commit', '-m', f'chore: brew formula update for {repo_name} {version}']  # noqa
     # fmt: on
-    _run_git_subprocess(command, "Formula file committed successfully.")
+    _run_git_subprocess(command, "Assets committed successfully.")
 
 
 def push_git(homebrew_tap: str, homebrew_owner: str):
@@ -64,7 +64,7 @@ def push_git(homebrew_tap: str, homebrew_owner: str):
     # fmt: off
     command = ['git', '-C', get_working_dir(homebrew_tap), 'push', f'https://x-access-token:{GITHUB_TOKEN}@github.com/{homebrew_owner}/{homebrew_tap}.git']  # noqa
     # fmt: on
-    _run_git_subprocess(command, f"Formula file pushed successfully to {homebrew_tap}.")
+    _run_git_subprocess(command, f"Assets pushed successfully to {homebrew_tap}.")
 
 
 def _run_git_subprocess(command: list[str], debug_message: Optional[str] = None):
